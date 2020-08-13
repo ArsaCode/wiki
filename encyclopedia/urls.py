f733a938abc39729b82e216e53bbdd6057fabc2c
Arsalan Ghassemi <contact@arsadevs.com>
2020-08-13 23:16:45 +0200

    Added the search page feature

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name="encyclopedia"
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.page, name="title")
+    path("wiki/<str:title>", views.title, name="title"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,19 +1,60 @@
 from django.shortcuts import render
 import markdown2
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
+
+class SearchForm(forms.Form):
+    search = forms.CharField(label="Search",
+                            max_length=100,
+                            widget= forms.TextInput
+                            (attrs={'class':'search',
+				            'placeholder':'Search Encyclopedia'
+                            }))
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
+        "form": SearchForm()
     })
 
-def page(request, title):
+def title(request, title):
     if util.get_entry(title):
         content = markdown2.markdown(util.get_entry(title))
         return render(request, "encyclopedia/page.html", {
         "title": title,
-        "page": content
+        "page": content,
+        "form": SearchForm()
         })
     else:
         return render(request, "encyclopedia/404.html")
+
+def search(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search = form.cleaned_data["search"]
+            if util.get_entry(search):
+                return HttpResponseRedirect(f"wiki/{ search }")
+            for entry in util.list_entries():
+                if search.lower() in entry.lower():
+                    searchlist = []
+                    searchlist.append(entry)
+                    return render(request, "encyclopedia/search.html", {
+                        "search": search,
+                        "searchlist": searchlist,
+                        "form": SearchForm()
+                    })
+            else:
+                return render(request, "encyclopedia/404.html", {
+                    "form": form
+            })
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "form": form
+            })
+    return render(request, "encyclopedia/index.html", {
+        "form": SearchForm()
+    })
